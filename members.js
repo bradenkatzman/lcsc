8e97e9c9f2c0a71aa22e96c0b918e6562988e3ab
bradenkatzman <bradenkatzman@gmail.com>
2015-08-04 23:42:35 -0700

    added bios, moved members to external js, added positions docx dowload link

---
+++ members.js
@@ -0,0 +1,52 @@
+	//separate positions objects for maintainability
+	var president = {
+		name:"noam",
+		bio:"&nbsp&nbspFollowing two years of experience on LCSC, Noam Kornsgold is ready to take on the responsibilities of the Presidency. Noam's strong work ethic and commitment to the List College student body will act as assets throughout the year. On campus, Noam is a brother of the Alpha Epsilon Pi Fraternity and an active member and former Va'ad member of the Koach group at Hillel. Throughout the year, Noam works for the Rabbinical Assembly, the rabbinic arm of the Conservative Movement. During the past few summers (and in future summers), Noam worked as a counselor at Camp Ramah in the Berkshires. Noam grew up in East Windsor, NJ. He attended the Ramaz Upper School for high school.<br><br>&nbsp&nbspThe President is the presiding office of LCSC and is responsible for overseeing all other Council members. The President is the liaison between LCSC and the various offices and departments of JTS. Although the President does not have a delineated portfolio, it is the President's job to ensure that LCSC is running as smoothly as possible and that all of its members are performing their tasks to the best of their abilities.<br><br>&nbsp&nbspAs President of LCSC 2015-2016, Noam intends to kindle more positive relationships between the JTS administration and List College students as well as to help the student body navigate the difficult 'swing period,' the time between the start of the building project and its conclusion. Some of Noam's policy goals include reducing the number of emails sent from JTS, obtaining 24/6 access to the JTS computer lab (or wherever the computers will be located during the swing period), and ensuring that the student body has a say in the approved designs for the building project. On the student life level, Noam wants to roll out a new line of List College apparel and create a peer major advising program. Finally, Noam wishes to make LCSC's budget more financially sound and improve on the communications between LCSC and the student body.<br><br>&nbsp&nbspNoam's favorite parts of being on LCSC are the relationships that are formed as a result of the job. LCSC members are able to interact with all students and administrators of JTS, which helps foster a sense of community around the school. Noam is extremely excited to be working as President on LCSC this year. He is ready to work with the new competent Council in order to make List College students' experiences at List College as pleasant and fun as possible.",
+		div:"hiddenDivTop1"
+	};
+	var vpPolicy = {
+		name:"harry",
+		bio:"policy",
+		div:"hiddenDivTop1"
+	};
+	var vpSL = {
+		name:"sam",
+		bio:"student life",
+		div:"hiddenDivTop2"
+	};
+	var vpFinance = {
+		name:"ross",
+		bio:"finance",
+		div:"hiddenDivTop2"
+	};
+	var vpComm = {
+		name:"braden",
+		bio:"communciations",
+		div:"hiddenDivTop3"
+	};
+
+	var SCP = {
+		name:"missy",
+		bio:"&nbsp&nbspHi List College! My name is Missy Zedeck and I am the senior class president on List College Student Council. I am a rising senior majoring in psychology at CU and modern Jewish studies at JTS. Being new to council, I am so excited to help plan events that will allow everyone to have an amazing year! My responsibilities as senior class president include planning events for the senior class and being a liaison between the administration and my class. Some goals that I have include planning some awesome events for the senior class, as well as helping the senior class make a lasting impact on the underclassmen.<br><br>&nbsp&nbspOutside of List College, I am a member of the Alpha Chi Omega sorority, serving as the assistant of PR/Marketing. I am also the main coordinator for After Hours Tutoring, one of the many clubs under Community Impact, which tutors and helps students with their homework at the American Regent Temporary Residency on 103rd st.<br><br>&nbsp&nbspI am really looking forward to being on council this year.",
+		div:"hiddenDivBottom1"
+	};
+	var JCP = {
+		name:"max",
+		bio:"&nbsp&nbspHi List College!!! I’m Max Rettig, the Junior Class President on List College Student Council for 2015-2016. At Columbia, I’m majoring in Creative Writing, and at List College, I’m majoring in Modern Jewish Studies with an elective concentration in Ethics. I’m active on both campuses — in my roles on LCSC and in the general List College community — and at Columbia, as a writer for Bwog (Columbia’s preeminent student news forum) and a sports commentator for WKCR, Columbia’s FM radio station (Tune in!). I’ve loved immersing myself in these different communities and only look to get even more engaged on campus. I also love to travel and explore New York City by foot and subway, visiting different neighborhoods, trying out new cafés and simply walking around and gathering the sights and sounds of the city. Oh, and when I get the chance, taking the “7” train to Queens for a fun day watching the Mets. <br><br>&nbsp&nbspAs Junior Class President, I’m responsible for being a liaison and a voice for the members of the junior class. In addition, my other responsibilities include holding monthly events that bring my entire class together to forge closer friendships and enjoy new experiences. Some of my goals include holding Shabbat potluck lunches and/or dinners for my entire class — an easy, fun and meaningful way to talk, learn and enjoy each other’s company — and having events that take us out of the Morningside Bubble and get the junior class excited, involved and wanting to see a new part of the city. I also strive to create a modern system of class-wide communication and engagement — goodbye long, boring emails, hello short weekly updates and fun, informative tweets!<br><br>&nbsp&nbspThis is my first year on LCSC, so most of all, I’m excited to meet new people, become closer with friends, be a loud and advocating voice for the interests of my peers. I want to help create a List College Student Council that students want to talk with, trust and become involved in themselves to make learning and playing at List College a better experience for everyone.",
+		div:"hiddenDivBottom1"
+	};
+	var sophCP = {
+		name:"marc",
+		bio:"&nbsp&nbspHello List College! I'm responsible for engaging the members of my class with one another through communal activities and expressing the interest of my class to the greater council. Through my monthly events, I hope to cultivate a sense of appreciation for my classmates' talents and unfound connection between students. In addition, I'm working to incorporate an element of service into the opportunities I provide and bring my class' community outside the walls of List College to inhabit surrounding spaces.<br><br>&nbsp&nbspI most recently served as a staff member for The JustCity Leadership Institute, the pre-college program of JTS, and social media coordinator for a minyan at the Columbia/Barnard Hillel, for which I am currently a service coordinator. I am always conscious to use what I know about Jewish leadership and my fellow classmates not only to represent them, but also to mobilize them to build unity.",
+		div:"hiddenDivBottom2"
+	};
+	var FCP = {
+		name:"fcp",
+		bio:"freshman class president",
+		div:"hiddenDivBottom2"
+	};
+	var DDR = {
+		name:"rebecca",
+		bio:"&nbsp&nbspRebecca Galin is a Senior at List College majoring in Film Studies and Prayer Studies.  This is her third year on council.  As the Double Degree Representative she deals with all things DD: policy issues at Barnard and JTS, monthly events, DD mentors, and more.  Some of her goals this year include building up a list of accepted current JTS classes that count for 9 Ways of Knowing at Barnard and creating DD traditions at JTS that build off the energy of our wonderful Barnard traditions (such as Convocation and Midnight Breakfast).  Rebecca loves binge watching shows on Netflix, cooking with strange vegetables from the Morningside Heights CSA, and service hopping at Hillel.",
+		div:"hiddenDivBottom3"
+	};
